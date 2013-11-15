533ff363be8797f71adb389505c04678a19592b2
Niket Patel <niket@niketpatel.com>
2013-11-15 17:55:17 +0530

    Reorganize test code

--- form_test.go
+++ form_test.go
@@ -10,6 +10,75 @@ import (
 	"testing"
 )
 
+func TestMultipartForm(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) { testReq(t, w, r) }
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	f := Form{}
+	f.Field().Add("Name", "John")
+	f.File().Add("File", "test-files/logo.png")
+
+	req, err := f.Request("POST", ts.URL+"/?test=true")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := http.DefaultClient.Do(req); err != nil {
+		t.Fatal(err)
+	}
+
+	contentType := req.Header.Get("Content-Type")
+	if contentType == "application/x-www-form-urlencoded" {
+		t.Fail()
+	}
+
+	if _, err := f.Do("POST", ts.URL+"/?test=true"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPostForm(t *testing.T) {
+	testForm("POST", t)
+}
+
+func TestPutForm(t *testing.T) {
+	testForm("PUT", t)
+}
+
+func TestGetForm(t *testing.T) {
+	testForm("GET", t)
+}
+
+func testForm(verb string, t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) { testReq(t, w, r) }
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	f := NewForm(nil, nil)
+	f.Field().Add("Name", "John")
+	f.Field().Add("Likes", "Ice Cream")
+	f.Header().Add("Host", "example.com")
+
+	req, err := f.Request(verb, ts.URL+"/?test=true")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if req.Method != verb {
+		t.Log(req.Method)
+		t.Fail()
+	}
+
+	if req.Header.Get("Host") != "example.com" {
+		t.Fail()
+	}
+
+	if _, err := http.DefaultClient.Do(req); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func testReq(t *testing.T, w http.ResponseWriter, r *http.Request) {
 	ctype := r.Header.Get("Content-Type")
 	m := make(url.Values)
@@ -50,42 +119,6 @@ func testReq(t *testing.T, w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func TestPostForm(t *testing.T) {
-	testForm("POST", t)
-}
-
-func TestPutForm(t *testing.T) {
-	testForm("PUT", t)
-}
-
-func TestMultipartForm(t *testing.T) {
-	handler := func(w http.ResponseWriter, r *http.Request) { testReq(t, w, r) }
-	ts := httptest.NewServer(http.HandlerFunc(handler))
-	defer ts.Close()
-
-	f := Form{}
-	f.Field().Add("Name", "John")
-	f.File().Add("File", "test-files/logo.png")
-
-	req, err := f.Request("POST", ts.URL+"/?test=true")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if _, err := http.DefaultClient.Do(req); err != nil {
-		t.Fatal(err)
-	}
-
-	contentType := req.Header.Get("Content-Type")
-	if contentType == "application/x-www-form-urlencoded" {
-		t.Fail()
-	}
-
-	if _, err := f.Do("POST", ts.URL+"/?test=true"); err != nil {
-		t.Fatal(err)
-	}
-}
-
 func TestFailedCase(t *testing.T) {
 	f := Form{}
 	f.Field().Add("Name", "John")
@@ -93,39 +126,6 @@ func TestFailedCase(t *testing.T) {
 
 	_, err := f.Request("POST", "http://local/")
 	if !os.IsNotExist(err) {
-		t.Fatal(err)
-	}
-}
-
-func TestGetForm(t *testing.T) {
-	testForm("GET", t)
-}
-
-func testForm(verb string, t *testing.T) {
-	handler := func(w http.ResponseWriter, r *http.Request) { testReq(t, w, r) }
-	ts := httptest.NewServer(http.HandlerFunc(handler))
-	defer ts.Close()
-
-	f := NewForm(nil, nil)
-	f.Field().Add("Name", "John")
-	f.Field().Add("Likes", "Ice Cream")
-	f.Header().Add("Host", "example.com")
-
-	req, err := f.Request(verb, ts.URL+"/?test=true")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if req.Method != verb {
-		t.Log(req.Method)
-		t.Fail()
-	}
-
-	if req.Header.Get("Host") != "example.com" {
-		t.Fail()
-	}
-
-	if _, err := http.DefaultClient.Do(req); err != nil {
 		t.Fatal(err)
 	}
 }
